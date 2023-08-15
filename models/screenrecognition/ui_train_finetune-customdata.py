18100aa2560d48b495f2d0bd74f2fb096c5fd541
YugyeomKim <hoodie0512@gmail.com>
2023-08-16 00:52:43 +0900

    deviices='auto' for Trainer

--- models/screenrecognition/ui_train_finetune-customdata.py
+++ models/screenrecognition/ui_train_finetune-customdata.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     trainer = Trainer(
         # gpus=1,
         accelerator="gpu",
-        devices=1,
+        devices='auto',
         gradient_clip_val=1.0,
         accumulate_grad_batches=2,
         callbacks=[checkpoint_callback, checkpoint_callback2, earlystopping_callback],
